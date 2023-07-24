61d5dc8562ae47b1e1a90fcfa7bec2e9314c6a96
Jignesh <jigsvadiyatar6557@gmail.com>
2023-07-24 16:19:52 +0530

    New Backend

--- config/google.js
+++ config/google.js
@@ -6,7 +6,7 @@ passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "https://thriving-raindrop-11b45e.netlify.app/auth/google/callback",
-    callbackURL: "http://localhost:8090/auth/google/callback",
+    callbackURL: "https://setcal-backend.onrender.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
